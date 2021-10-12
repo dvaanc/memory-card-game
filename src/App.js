91f3ea9c118462e587a3a4c0afc1cc84b336a1a7
Danny C PC <dannyc52521@gmail.com>
2021-10-13 08:11:43 +1100

    added logic

--- src/App.js
+++ src/App.js
@@ -8,9 +8,51 @@ function App() {
   const [cards, setCards] = React.useState(list);
   const [score, setScore] = React.useState(0);
   const [record, setRecord] = React.useState(0);
-  function cardClicked(e) {
+  function handleCardClicked(e) {
     e.preventDefault();
-    console.log(e);
+    if(e.target.parentNode.parentNode.classList.contains('card')) {
+      const parent = e.target.parentNode.parentNode;
+      const id = parent.id;
+      if(checkCardClicked(id)) {
+
+      }
+    } 
+    if(e.target.parentNode.classList.contains('card')) {
+      const parent = e.target.parentNode;
+      const id = parent.id;
+      checkCardClicked(id)
+
+    }
+    if(e.target.classList.contains('card')) {
+      const parent = e.target;
+      const id = parent.id;
+      checkCardClicked(id)
+    }
+  }
+  function checkCardClicked(id) {
+    console.log(cards.some((item) => {
+      return item.clicked && item.name === id;
+    }))
+  }
+  function setCardClicked(id) {
+    const card = cards.filter((item) => item.name === id)
+    console.log(card)
+    Promise.resolve()
+    .then(() => {
+      setCards({
+        ...cards, 
+        card
+      })
+    })
+    .then(() => {
+      console.log(cards)
+    })
+  }
+  function resetGame() {
+
+  }
+  function shuffleCards() {
+    
   }
 
   return (
@@ -19,14 +61,18 @@ function App() {
         <h1>Memory Card Game</h1>
       </header>
       <div className="body">
+      <div className="score">
+        <p>Score:</p>
+        <p>Record:</p>
+      </div>
       <div className="cardContainer">
       {
         cards.map((item) => {
           const uuid = uuidv4();
         return (
-          <div className="card" key={uuid} onClick={cardClicked}>
+          <div className="card" key={uuid} id={item.name} onClick={handleCardClicked}>
             <div className="imgContainer">
-              <img src={item.src} alt="angular"/>
+              <img src={item.src} alt="angular" draggable="false"/>
             </div>
             <p>{item.name}</p>
           </div>
